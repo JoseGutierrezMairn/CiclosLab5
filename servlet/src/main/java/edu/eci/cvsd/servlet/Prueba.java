db954405d9aa5193714722b49f4f4e306daa644d
unknown <jonathan.pulido@labinfo.is.escuelaing.edu.co>
2020-02-25 09:27:23 -0500

    Avance parte 2

--- servlet/src/main/java/edu/eci/cvsd/servlet/Prueba.java
+++ servlet/src/main/java/edu/eci/cvsd/servlet/Prueba.java
@@ -41,5 +41,27 @@ public class Prueba extends HttpServlet {
 			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 		}
 	   }
+	
+	@Override
+	   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Writer responseWriter = resp.getWriter();
+		String id=req.getParameter("id");
+		try {
+			Porhacer=Service.getTodo(Integer.parseInt(id));
+			Lista.add(Porhacer);
+			resp.setStatus(HttpServletResponse.SC_OK);
+		    responseWriter.write(Service.todosToHTMLTable(Lista));
+		    responseWriter.flush();
+		}
+		catch(NullPointerException e) {
+			resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+		catch(MalformedURLException e) {
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+		catch(Exception e) {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		}
+	   }
 
 }
