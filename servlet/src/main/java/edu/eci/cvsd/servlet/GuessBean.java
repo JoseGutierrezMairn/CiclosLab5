f05fb85942e8e3e0bd0e27fed7d83a116da36a87
unknown <andres.gutierrez-m@labinfo.is.escuelaing.edu.co>
2020-02-28 17:54:20 -0500

    Parte 4

--- servlet/src/main/java/edu/eci/cvsd/servlet/GuessBean.java
+++ servlet/src/main/java/edu/eci/cvsd/servlet/GuessBean.java
@@ -2,15 +2,14 @@ package edu.eci.cvsd.servlet;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.faces.bean.*;
+import javax.faces.bean.ManagedBean;
 
 
-import javax.annotation.ManagedBean;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.SessionScoped;
 
-@ManagedBean(name = "guessBean")
-@ApplicationScoped
-
+@ManagedBean(name = "GuessBean")
+@SessionScoped
 public class GuessBean {
 	
 	private int intentos;
@@ -18,42 +17,72 @@ public class GuessBean {
 	private int premio;
 	private String estado;
 	private String fallidos;
-	
-	
-	public GuessBean() {
+	private int numeroUsuario;
 		
+		
+	public GuessBean() {
+		restart();
+			
 	}
-	
+		
+	public String getFallidos() {
+		return fallidos;
+	}
+
+	public void setFallidos(String fallidos) {
+		this.fallidos = fallidos;
+	}
+
+	public int getNumeroUsuario() {
+		return numeroUsuario;
+	}
+
+	public void setNumeroUsuario(int numeroUsuario) {
+		this.numeroUsuario = numeroUsuario;
+	}
+
 	public void guess(int numeroUsuario){
-        
-        if(premio > 0 && estado!="gano" && numeroUsuario>=0 && numeroUsuario<=20){
-            if(numeroUsuario == numero){
-                intentos++;
-                estado="gano";
-            }else{
-                premio-=10000;
-                intentos++;
-                estado="fallo";
-                
-                fallidos += " " + String.valueOf(numeroUsuario)+",";
-            }
-        }else{
-            if(estado!="gano" && numeroUsuario>=0 && numeroUsuario<=20 ){
-                estado="Perdio y no tiene mas intentos.";
-            }
+	    
+	    if(premio > 0 && estado!="gano" && numeroUsuario>=0 && numeroUsuario<=20){
+	    	if(numeroUsuario == numero){
+	    		intentos++;
+	            estado="gano";
+	        }else{
+	        	premio-=10000;
+	        	intentos++;
+	        	estado="fallo";
+	        	          
+	            fallidos += " " + String.valueOf(numeroUsuario)+",";
+	        }
+	        }else{
+	        	
+	            if(estado!="gano" && numeroUsuario>=0 && numeroUsuario<=20 ){
+	            	
+	                estado="Perdio y no tiene mas intentos.";
+	            }
+	        }
+	 }
+	
+	public void guess(String numeroUsuario){
+        try{
+            this.numeroUsuario = Integer.parseInt(numeroUsuario);
+            guess(this.numeroUsuario);
+            
+        }catch(Exception e){
+            estado = "Valor invalido, solo se pueden digitar numeros enteros.";
         }
     }
 	
-	
+		
 	public void restart(){
-        Random rand = new Random();
-        numero = rand.nextInt(21);
-        intentos = 0;
-        premio = 100000;
-        estado = "Tiene que adivinar un numero entero entre 0 y 20.";
-        fallidos = "";
-        
-    }
+	        Random rand = new Random();
+	        numero = rand.nextInt(21);
+	        intentos = 0;
+	        premio = 100000;
+	        estado = "Tiene que adivinar un numero entero entre 0 y 20.";
+	        fallidos = "";
+	        
+	}
 	public int getIntentos() {
 		return intentos;
 	}
@@ -86,5 +115,5 @@ public class GuessBean {
 		this.estado = estado;
 	}
 
-	
+
 }
