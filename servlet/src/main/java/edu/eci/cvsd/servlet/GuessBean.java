ea93406cb2adfa85421f38fa425598a8cd227310
unknown <andres.gutierrez-m@labinfo.is.escuelaing.edu.co>
2020-02-28 11:29:31 -0500

    Adelanto parte 4

---
+++ servlet/src/main/java/edu/eci/cvsd/servlet/GuessBean.java
@@ -0,0 +1,90 @@
+package edu.eci.cvsd.servlet;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.faces.bean.*;
+
+
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
+
+@ManagedBean(name = "guessBean")
+@ApplicationScoped
+
+public class GuessBean {
+	
+	private int intentos;
+	private int numero;
+	private int premio;
+	private String estado;
+	private String fallidos;
+	
+	
+	public GuessBean() {
+		
+	}
+	
+	public void guess(int numeroUsuario){
+        
+        if(premio > 0 && estado!="gano" && numeroUsuario>=0 && numeroUsuario<=20){
+            if(numeroUsuario == numero){
+                intentos++;
+                estado="gano";
+            }else{
+                premio-=10000;
+                intentos++;
+                estado="fallo";
+                
+                fallidos += " " + String.valueOf(numeroUsuario)+",";
+            }
+        }else{
+            if(estado!="gano" && numeroUsuario>=0 && numeroUsuario<=20 ){
+                estado="Perdio y no tiene mas intentos.";
+            }
+        }
+    }
+	
+	
+	public void restart(){
+        Random rand = new Random();
+        numero = rand.nextInt(21);
+        intentos = 0;
+        premio = 100000;
+        estado = "Tiene que adivinar un numero entero entre 0 y 20.";
+        fallidos = "";
+        
+    }
+	public int getIntentos() {
+		return intentos;
+	}
+
+	public void setIntentos(int intentos) {
+		this.intentos = intentos;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getPremio() {
+		return premio;
+	}
+
+	public void setPremio(int premio) {
+		this.premio = premio;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	
+}
